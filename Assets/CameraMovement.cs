3b5f2b4e0256e70c60a07f021ca8e585ad91f568
dhneverdie <583656269@qq.com>
2022-01-15 22:24:48 +0800

    按照教程写的，适用于RTS游戏的可操控相机

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    float speed = 0.05f;
+    float zoomSpeed = 10.0f ;
+    float rotateSpeed = 0.1f;
+
+    private float maxHeight = 40f, minHeight = 4f;
+
+    private Vector2 p1, p2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 0.06f;
+            zoomSpeed = 20f;
+        }
+        else
+        {
+            speed = 0.035f;
+            zoomSpeed = 10f;
+        }
+        
+        float hsp = transform.position.y *  speed * Input.GetAxis("Horizontal");
+        float vsp = transform.position.y * speed * Input.GetAxis("Vertical");
+        float scrollSp = Mathf.Log(transform.position.y) * -zoomSpeed * Input.GetAxis("Mouse ScrollWheel");
+        if (transform.position.y >= maxHeight && (scrollSp > 0))
+        {
+            scrollSp = 0;
+        }
+        else if(transform.position.y <= minHeight && (scrollSp < 0))
+        {
+            scrollSp = 0; 
+        }
+
+        if (transform.position.y + scrollSp >= maxHeight )
+        {
+            scrollSp =maxHeight -transform.position.y ;
+        }else if (transform.position.y + scrollSp <= minHeight)
+        {
+            scrollSp =minHeight - transform.position.y;
+        }
+        Vector3 verticalMove = new Vector3(0, scrollSp, 0);
+        Vector3 lateralMove = hsp * transform.right;
+        Vector3 forwardMove = transform.forward;
+        forwardMove.y = 0;
+        forwardMove.Normalize();
+        forwardMove *= vsp;
+        Vector3 move = verticalMove + lateralMove + forwardMove;
+        transform.position += move;
+
+
+        getCameraRotation();
+    }
+
+    void getCameraRotation()
+    {
+        if (Input.GetMouseButtonDown((2)))
+        {
+            p1 = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(2))
+        {
+            p2 = Input.mousePosition;
+            float dx = (p2 - p1).x * rotateSpeed;
+            float dy = (p2 - p1).y * rotateSpeed;
+
+            transform.rotation *= Quaternion.Euler(new Vector3(0, dx, 0));
+            transform.GetChild(0).transform.rotation *= Quaternion.Euler(new Vector3(-dy,0,0));
+            p1 = p2;
+        }
+    }
+}
